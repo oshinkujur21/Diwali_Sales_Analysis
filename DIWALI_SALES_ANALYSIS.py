5f93ee34e70c22efe58dfff23dcff53301b22f67
Oshin Catherein Kujur <oshinkujur69@gmail.com>
2025-03-31 19:43:19 +0530

    Update DIWALI_SALES_ANALYSIS.py

--- DIWALI_SALES_ANALYSIS.py
+++ DIWALI_SALES_ANALYSIS.py
@@ -1,12 +1,10 @@
-# import python libraries
-
 import numpy as np 
 import pandas as pd 
-import matplotlib.pyplot as plt # visualizing data
+import matplotlib.pyplot as plt 
 %matplotlib inline
 import seaborn as sns
 
-# import csv file
+
 df = pd.read_csv('Diwali Sales Data.csv', encoding= 'unicode_escape')
 
 df.shape
@@ -15,29 +13,29 @@ df.head(10)
 
 df.info()
 
-#drop unrelated/blank columns
+
 df.drop(['Status', 'unnamed1'], axis=1, inplace=True)
 
-#check for null values
+
 pd.isnull(df).sum()
 
-# drop null values
+
 df.dropna(inplace=True)
 
-# change data type
+
 df['Amount'] = df['Amount'].astype('int')
 
 df['Amount'].dtypes
 
 df.columns
 
-#rename column
+
 df.rename(columns= {'Marital_Status':'Shaadi'})
 
-# describe() method returns description of the data in the DataFrame (i.e. count, mean, std, etc)
+
 df.describe()
 
-# use describe() for specific columns
+
 df[['Age', 'Orders', 'Amount']].describe()
 
 # Exploratory Data Analysis
